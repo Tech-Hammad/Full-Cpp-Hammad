7d0085c39a966fb8345c036e71db73d79b40b98d
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-02-20 01:42:44 +0500

    Add files via upload

---
+++ Programming Fundamentals C++/02-Data_Types.cpp
@@ -0,0 +1,31 @@
+// This Code Contain Size of Each Data Type in C++
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a;
+    cout << "Size of int is " << sizeof(a) << endl;
+
+    short int si;
+    cout << "Size of short int is " << sizeof(si) << endl;
+    ;
+
+    long int li;
+    cout << "Size of long int is " << sizeof(li) << endl;
+
+    char b;
+    cout << "Size of char is " << sizeof(b) << endl;
+
+    float c;
+    cout << "Size of float is " << sizeof(c) << endl;
+
+    double d;
+    cout << "Size of double is " << sizeof(d) << endl;
+
+    bool e;
+    cout << "Size of bool is " << sizeof(e) << endl;
+    
+    return 0;
+}
---
+++ Programming Fundamentals C++/06-Switch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int num;
+    cout<<"Enter shirt number"<<endl;
+    cin>>num;
+
+    switch (num)
+    {
+        case 169:
+            cout<<"Hammad";
+        break;
+        case 56:
+            cout<<"Babar";
+        break;
+        case 18:
+            cout<<"Virat";
+        break;
+        case 10:
+            cout<<"Shaheen";
+        break;
+        default:
+            cout<<"Not found";
+        break;
+
+    }
+    return 0;
+}
