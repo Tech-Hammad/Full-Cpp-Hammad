c5b085d8de2c8b4dda94a2907c37d0ffac546242
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-03-03 19:43:09 +0500

    Add files via upload

---
+++ Programming Fundamentals C++/51_Copy_Strings.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void CopyString(char*& dest, const char* source)
+{
+	int i;
+	int size = strlen(source);
+	dest = new char[size + 1];
+	for (i = 0; source[i] != '\0'; i++)
+	{
+		dest[i] = source[i];
+	}
+	dest[i] = '\0';  // Null Terminated String
+}
+
+int main()
+{
+	char* destString = nullptr;
+	char* sourceString = "Hello,I am Hammad,I Love Coding!";
+
+	CopyString(destString, sourceString);
+
+	cout << "Original String: " << sourceString << "\n";
+	cout << "Copied String: " << destString << "\n";
+
+	return 0;
+}
