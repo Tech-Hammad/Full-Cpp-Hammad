7a196adc60485770d8eb78b038dc2cc35b51a67b
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-03-05 23:37:10 +0500

    Update 07-Loops

--- Programming Fundamentals C++/07-Loops.cpp
+++ Programming Fundamentals C++/07-Loops.cpp
@@ -1,24 +1,38 @@
+// This File Cover all 3 Loops used in C++
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int num;
-    cout << "Enter a number" << endl;
-    cin >> num;
-
-    int sum = 0;
-    for (int i = 1; i <= num; i++)
+    char choice;
+    do 
     {
-        sum = sum + i;
-    }
+        int n;
+        cout << "Enter a positive number: ";
+        cin >> n;
 
-    while (num > 0)
-    {
-        cout << num << endl;
-        cin >> num;
-    }
+        // Using for loop to print numbers from 1 to n
+        cout << "Numbers from 1 to " << n << ": ";
+        for (int i = 1; i <= n; i++) 
+        {
+            cout << i << " ";
+        }
+        cout << endl;
 
-    cout << sum << endl;
+        // Using while loop to print even numbers up to n
+        cout << "Even numbers up to " << n << ": ";
+        int i = 2;
+        while (i <= n)
+        {
+            cout << i << " ";
+            i = i+2;
+        }
+        cout << endl;
+        cout << "Do you want to try again? (Yes/No): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y'); // Using do while loop for repetition
+
+    cout << "Program exited successfully.\n";
     return 0;
 }
