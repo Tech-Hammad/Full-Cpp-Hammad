bc75f0a75e380d9ce80ed2732bc9b35ea867ae5c
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-03-08 17:10:09 +0500

    Update 15-pascalTriangle.cpp

--- Programming Fundamentals C++/15-pascalTriangle.cpp
+++ Programming Fundamentals C++/15-pascalTriangle.cpp
@@ -1,16 +1,13 @@
 // Pascal Triangle
-
 #include <iostream>
 using namespace std;
 
 int fact(int n)
 {
-
     int factorial = 1;
-
     for (int i = 2; i <= n; i++)
     {
-        factorial *= i;
+        factorial = factorial * i;
     }
     return factorial;
 }
@@ -30,10 +27,11 @@ int main()
     }
     return 0;
 }
+
 /*
-1
-1 1
-1 2 1
-1 3 3 1
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
 1 4 6 4 1
 */
