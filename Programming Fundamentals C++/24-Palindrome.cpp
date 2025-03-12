8e6254072d71a56c473155b4ac5f142924670ce4
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-03-12 17:49:26 +0500

    Update Palindrome.cpp

--- Programming Fundamentals C++/24-Palindrome.cpp
+++ Programming Fundamentals C++/24-Palindrome.cpp
@@ -5,34 +5,31 @@ using namespace std;
 
 int main()
 {
-    // --NOTE--
-    // Variable-length arrays may cause errors in some compilers.
-    // If your compiler does not support this, use dynamic memory allocation (e.g., 'new') or a fixed-size array.
-
     int n;
     cout << "Enter size of array :";
     cin >> n;
 
     char arr[n + 1];
+    cout<< "Enter " << n+1 << " Elements:-";
     cin >> arr;
 
-    bool check = 1;
+    bool check = true;
 
     for (int i = 0; i < n; i++)
     {
         if (arr[i] != arr[n - 1 - i])
         {
-            check = 0;
+            check = false;
             break;
         }
     }
     if (check == true)
     {
-        cout << "Palindrome";
+        cout << "It is Palindrome";
     }
     else
     {
-        cout << "Not Palindrome";
+        cout << "It is Not Palindrome";
     }
     return 0;
 }
