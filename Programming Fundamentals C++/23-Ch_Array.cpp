a2f966e041cfb95f066fdeb84b95e381bf7d5abe
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-03-12 17:44:18 +0500

    Update Ch_Array.cpp

--- Programming Fundamentals C++/23-Ch_Array.cpp
+++ Programming Fundamentals C++/23-Ch_Array.cpp
@@ -3,21 +3,20 @@ using namespace std;
 
 int main()
 {
-
     char arr[100] = "Hammad";
-
-    int i = 0;
+    int i = 0;  // Iterator
 
     while (arr[i] != '\0')
     {
         cout << arr[i];
         i++;
     }
+    arr[i]='\0';  // Null Terminate the Array
     cout << endl;
 
     // input and output in chr array
     char arr2[100];
-
+    cout<< "Input Your Element in Array:-";
     cin >> arr;
 
     cout << arr;
