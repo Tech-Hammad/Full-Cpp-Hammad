3f1d4c380a7a6705b435e7d2f7437c7d7f848acd
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-03-14 20:19:35 +0500

    Update Transpose.cpp

--- Programming Fundamentals C++/28-Transpose.cpp
+++ Programming Fundamentals C++/28-Transpose.cpp
@@ -1,23 +1,34 @@
 // We have given a square matric find its transpose
-
 #include <iostream>
 using namespace std;
 int main()
 {
     int arr[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
+    cout << "Original Array:-"  << endl;
+    for(int i = 0; i < 3; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            cout<<arr[i][j] << " ";
+        }
+        cout<< endl;
+    }
+
+    // Taking Transpose
     for (int i = 0; i < 3; i++)
     {
         for (int j = i + 1; j < 3; j++)
         {
-            //   for swap
+            // Swapping
             int temp = arr[i][j];
             arr[i][j] = arr[j][i];
             arr[j][i] = temp;
         }
     }
-
-    // printing
+    cout << endl;
+    // Display Transposed Array
+    cout << "Transposed Array:-" << endl;
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
