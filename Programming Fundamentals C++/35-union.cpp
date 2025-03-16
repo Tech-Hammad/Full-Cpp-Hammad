c2ca953c852dd83fdd0d6113d78e2362a54872fe
Hammad Ahmad <hammadahmad.9022@gmail.com>
2025-03-16 13:44:19 +0500

    Update union.cpp

--- Programming Fundamentals C++/35-union.cpp
+++ Programming Fundamentals C++/35-union.cpp
@@ -1,23 +1,25 @@
 // UNION and ENUM
-
 #include <iostream>
 using namespace std;
 
 union players
-{                 // union mein eek wqt mein eek hi use kr skty hain
-    int num;      // 4 byte
-    char favChar; // 1 byte
-    float score;  // 4 byte
+{   
+    // In a union, only one member can be used at a time because memory is shared.
+    int num;        // 4 byte
+    char favChar;   // 1 byte
+    float score;    // 4 byte
 };
-// agr ye structure hota tou total memory 9 byte but in union memeory is shared so total is 4 byte ye memory allocate krta ha max jo ho utni
+// If this were a structure, the total memory allocated would be 9 bytes.
+// However, in a union, memory is shared, so the total allocated memory is equal to the largest member, which is 4 bytes.
 
 int main()
 {
 
     union players p1;
     p1.num = 20;
-    p1.score = 169; // ab humny score b use kia tou num ki value garbage ajygi q k eek time mein 1 hi use kr skty hain
+    p1.score = 169;   // Assigning a value to 'score', which will overwrite 'num' due to shared memory
 
+    // Since memory is shared, accessing 'num' will now give garbage value
     cout << p1.num << endl;
 
     enum meal
@@ -25,14 +27,15 @@ int main()
         breakfast,
         lunch,
         dinner
-    }; // ye eek datatype bn gya ha ismy value store hogya ha
-
+    }; 
+    // This creates a new data type where the values are stored as integers starting from 0
+    
     meal m1 = breakfast;
 
-    cout << m1 << endl;
-    cout << breakfast << endl;
-    cout << lunch << endl;
-    cout << dinner << endl;
+    cout << m1 << endl;         // 0 since breakfast is the first value in the enum
+    cout << breakfast << endl;  // 0
+    cout << lunch << endl;      // 1
+    cout << dinner << endl;     // 2
 
     return 0;
 }
